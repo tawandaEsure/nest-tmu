a52d10510475685dd6c84352754ca0f43d74f022
Tawanda Kanyangarara <tawanda.kanyangarara@esure.com>
2021-01-21 06:43:47 +0000

    installing ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [HttpModule, TasksModule],
+  imports: [HttpModule, TasksModule, TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
